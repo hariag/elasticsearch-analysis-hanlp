438486315201e93342e4302d60dc0c212c6b5e67
Guo Haria <hariag@users.noreply.github.com>
2017-04-17 14:18:32 +0800

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,9 @@
 docker build -t haria/elasticsearch_analysis_hanlp:1.0 .
+sudo docker run --privileged=true -it haria/elasticsearch_analysis_hanlp:1.0
+
+# sudo docker ps -a
+# sudo docker rmi IMAGE_ID
+# sudo docker rm CONTAINER_ID
+# sudo docker ps -a|cut -d " " -f1|xargs -i sudo docker rm {}
+# sudo docker save IMAGE_ID > archive.tar
+# sudo docker load < archive.tar
